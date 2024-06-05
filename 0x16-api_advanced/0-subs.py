27fd5c95040b09f5b179f7f10cc616701d40bbc4
isaackwakye <isaaclove520@gmail.com>
2024-06-04 18:14:29 -0700

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent": "Mozilla/5.0"
+            ""User-Agent": "custom-user-agent""
             }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
